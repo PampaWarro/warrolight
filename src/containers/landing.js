a4f0fff0271a3c2fbf2ac16345b954099ff5d700
Esteban Ordano <eordano@gmail.com>
2017-01-06 16:11:11 -0800

    hot reloading

--- src/containers/landing.js
+++ src/containers/landing.js
@@ -2,40 +2,15 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 const randomcolor = require('randomcolor')
 
-import { Func as blinkFunc, config as blinkConfig} from '../function/blink'
-import { Func as blinkFunc2, config as blinkConfig2} from '../function/blink2'
-import { Func as rainbowFunc, config as rainbowConfig} from '../function/rainbow'
-import { Func as offFunc, config as offConfig} from '../function/turned-off'
-import { Func as volumeFunc, config as volumeConfig} from '../function/histogram'
+import { } from '../function/blink'
+import { } from '../function/blink2'
+import { } from '../function/rainbow'
+import { } from '../function/turned-off'
+import { } from '../function/histogram'
+
 import { LedSimulator } from './simulator-canvas'
 
-const Programs = {
-  'blink': {
-    config: blinkConfig,
-    func: blinkFunc,
-    name: 'Blink'
-  },
-  'blink2': {
-    config: blinkConfig2,
-    func: blinkFunc2,
-    name: 'Blink blanco'
-  },
-  'rainbow': {
-    config: rainbowConfig,
-    func: rainbowFunc,
-    name: 'Rainbow'
-  },
-  'off': {
-    config: offConfig,
-    func: offFunc,
-    name: 'Off'
-  },
-  'volumeLight': {
-    config: volumeConfig,
-    func: volumeFunc,
-    name: 'Volume light'
-  }
-}
+const ProgramNames = ['blink', 'blink2', 'rainbow', 'turned-off', 'histogram']
 
 class Item extends React.Component {
   render() {
@@ -46,8 +21,10 @@ class Item extends React.Component {
 export class Simulator extends React.Component {
   constructor() {
     super(...arguments)
-    this.state = { selected: ['rainbow'] }
-    this.func = new (Programs['rainbow'].func)()
+    const Programs = this.getPrograms();
+    const initial = 'blink'
+    this.state = { selected: [initial], Programs }
+    this.func = new (Programs[initial].func)()
   }
   handleClick(key, ev) {
     ev.preventDefault()
@@ -55,7 +32,7 @@ export class Simulator extends React.Component {
     this.setState({ selected: [key] })
 
     this.func.stop()
-    this.func = new (Programs[key].func)()
+    this.func = new (this.state.Programs[key].func)()
     const self = this
     this.func.start({
       frequencyInHertz: 0.02,
@@ -72,7 +49,23 @@ export class Simulator extends React.Component {
     }, function(leds) {
       self.updateLeds(leds)
     }, () => ({}))
+
+    this.setState({ Programs: this.getPrograms() })
   }
+
+  getPrograms() {
+    const Programs = {}
+    for (let program of ProgramNames) {
+      const mod = require('../function/' + program);
+      Programs[program] = {
+        name: program,
+        config: mod.config,
+        func: mod.Func
+      }
+    }
+    return Programs
+  }
+
   updateLeds(leds) {
     this.props.send(leds)
     if (this.refs.simulator) {
@@ -84,13 +77,13 @@ export class Simulator extends React.Component {
   }
   render() {
     var menuItems = [];
-    for (var key in Programs){
-      menuItems.push( <Item key={key} onClick={this.handleClick.bind(this, key)}>{Programs[key].name}</Item>)
+    for (var key in this.state.Programs){
+      menuItems.push( <Item key={key} onClick={this.handleClick.bind(this, key)}>{this.state.Programs[key].name}</Item>)
     }
     return (<div>
       <div>
         <h2>Simulator</h2>
-        <h3>Current Program: { Programs[this.state.selected[0]].name } </h3>
+        <h3>Current Program: { this.state.Programs[this.state.selected[0]].name } </h3>
         <ul>{ menuItems }</ul>
         <LedSimulator ref='simulator' />
       </div>

ade1f0b357a985ed152c38427c0cf9bae0417619
Esteban Ordano <eordano@gmail.com>
2017-02-17 18:53:14 -0800

    Communication to lights different from webpack

--- communication.js
+++ communication.js
@@ -16,24 +16,35 @@ const compiler = webpack(config);
 const Device = require('./device')
 const Multiplexer = require('./multiplexer')
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: "/"
-}));
-app.use(require('webpack-hot-middleware')(compiler));
+var device1, device2, device3, device4
+var multiplexer
 
-app.get('*', function (req, res, next) {
-  var filename = path.join(compiler.outputPath,'index.html');
-  compiler.outputFileSystem.readFile(filename, function(err, result){
-    if (err) {
-      return next(err);
+setTimeout(() => {
+  device1 = new Device(150, '/dev/ttyACM0')
+}, 1000)
+setTimeout(() => {
+  device2 = new Device(150, '/dev/ttyACM1')
+}, 2000)
+setTimeout(() => {
+  device3 = new Device(150, '/dev/ttyACM2')
+}, 3000)
+setTimeout(() => {
+  device4 = new Device(150, '/dev/ttyACM3')
+}, 4000)
+
+setTimeout(() => {
+  multiplexer = new Multiplexer(600, [device3, device1, device2, device4], (index) => {
+    if(index < 150) {
+      return [0, index]
+    } else if(index < 300){
+      return [1, index - 150]
+    } else if (index < 450) {
+      return [2, index - 300]
+    } else {
+      return [3, index - 450]
     }
-    res.set('content-type','text/html');
-    res.send(result);
-    res.end();
-  });
-})
-
+  })
+}, 5000)
 
 
 let djActionRunning = false;

f9e0f36f0d651070a891e95f8b366932bfa8326f
Esteban Ordano <eordano@gmail.com>
2017-02-22 21:25:27 -0800

    Add folder for functions that cant be used alone

--- src/containers/main.js
+++ src/containers/main.js
@@ -5,7 +5,7 @@ import { default as Geometry } from '../geometry/geometry'
 
 const ProgramNames = [
   'debugSetup', 'test1', 'heart', 'spear-blink',
-  'rainbow2', 'white-spear', 'fire-explosion',
+  'rainbow2', 'white-spear',
   'all-white', 'all-off', 'blink', 'rainbow', 'stars', 'musicFlow', 'musicFreqs', 'vertical', 'radial',
   'mixRainbowTriangulos', 'mixMusicW', 'mixMusicPsycho',
   'multiIntro', 'multiFlor', 'multiWarroLetras', 'fire'
@@ -38,7 +38,7 @@ export class Simulator extends React.Component {
     }
 
     const programs = this.programs = this.getPrograms();
-    const initial = 'fire-explosion';
+    const initial = 'fire';
     let initialConfig = this.getConfig(programs[initial].config);
     this.state = {
       selected: initial,
--- src/function/fire.js
+++ src/function/fire.js
@@ -1,59 +1,13 @@
-import {ColorUtils} from "../utils/ColorUtils";
-import {TimeTickedFunction} from "./TimeTickedFunction";
+// import {Func} from "./rainbow";
+const _ = require('lodash')
 
-export class Func extends TimeTickedFunction {
-  constructor(config, leds) {
-    super(config, leds);
+import {programsByShape} from "./Transformations";
 
-    this.colors = new Array(this.numberOfLeds)
-    this.bright = new Array(this.numberOfLeds)
-    this.currentBright = new Array(this.numberOfLeds)
+const Fire = require("./components/fire").Func;
 
-    for (let i = 0; i < this.numberOfLeds; i++) {
-      this.colors[i] = 0.18 - (this.position.y[i] / 200)
-      const height = this.position.y[i] / 20
-      this.bright[i] = height * height + 0.1
-      this.currentBright[i] = this.bright[i]
-    }
-    this.time = 0;
-  }
-
-  drawFrame(draw, done) {
-    this.time += this.config.speed;
-    const decay = this.config.decay
-    const chance = this.config.chance
-    const newColors = new Array(this.numberOfLeds)
-
-    for (let i = 0; i < this.numberOfLeds; i++) {
-      const color = this.colors[i]
-      let bright = this.currentBright[i] * decay
-      if (Math.random() > chance) {
-        bright = 1
-      }
-      this.currentBright[i] = bright
-      newColors[i] = ColorUtils.HSVtoHex(
-        color,
-        color > 0.13 ? 1 - color * 2 : 1,
-        this.config.brillo * this.bright[i] * bright,
-      )
-    }
-    draw(newColors);
-    done()
-  }
-
-  static presets() {
-    return {
-      fastMarks: {speed: 3, sameColorLeds: 5},
-    }
-  }
-
-  // Override and extend config Schema
-  static configSchema() {
-    let config = super.configSchema();
-    config.speed = {type: Number, min: 5, max: 20, default: 4};
-    config.decay = {type: Number, min: 0.98, max: 0.99999, step: 0.001, default: 0.985};
-    config.chance = {type: Number, min: 0.90, max: 0.99, step: 0.001, default: 0.95};
-    config.brillo = {type: Number, min: 0, max: 1, step: 0.01, default: 0.3};
-    return config;
-  }
+// las formas que se pueden usar están definidas en Transformation
+const mapping = {
+  "Warro": Fire,
 }
+
+export const Func = programsByShape(mapping)

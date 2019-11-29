7aa1d4138f5bc6b8a28a94cb7404151e5fa6d9cb
Federico Bond <federicobond@gmail.com>
2019-11-29 10:29:06 -0300

    Refactor ProgramList

--- web/src/ProgramList.jsx
+++ web/src/ProgramList.jsx
@@ -2,39 +2,21 @@ import React from "react";
 
 export class ProgramList extends React.Component {
     render() {
-      let menuItems = [];
-      for (let key in this.props.programs) {
-        if (key === this.props.selected) {
-          menuItems.push(
-            <Item key={key} selected>
-              {this.props.programs[key].name}
-            </Item>
-          );
-        } else {
-          menuItems.push(
-            <Item key={key} onClick={() => this.props.onProgramChange(key)}>
-              {this.props.programs[key].name}
-            </Item>
-          );
-        }
-      }
-  
       return (
         <ul className="nav flex-column">
-          {menuItems}
+          {Object.keys(this.props.programs).map((key) => {
+            let program = this.props.programs[key];
+            let selected = key === this.props.selected;
+
+            return (
+              <li key={key} className="nav-item">
+                <a href="#" className={"nav-link " + (selected ? "active" : "")} onClick={() => this.props.onProgramChange(key)}>
+                  {program.name}
+                </a>
+              </li>
+            )
+          })}
         </ul>
       )
-    }
-}
-
-class Item extends React.Component {
-    render() {
-      return (
-        <li className="nav-item">
-          <a href="#" className={"nav-link " + (this.props.selected ? "active" : "")} onClick={this.props.onClick}>
-            {this.props.children}
-          </a>
-        </li>
-      );
     }
 }

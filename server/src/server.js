103e2fde240e807cacaa6b7b417989f140577e25
Federico Bond <federicobond@gmail.com>
2019-11-29 11:53:33 -0300

    Extract Service from server

--- server/src/server.js
+++ server/src/server.js
@@ -1,68 +1,21 @@
 const _ = require('lodash');
 const WebSocket = require('ws');
-const { Buffer } = require('buffer');
 const EventEmitter = require('events');
 const { startMic } = require('./mic');
 const soundEmitter = require('./soundEmitter');
+const { Service, MicConfig, SoundSource } = require('./service')
 
 startMic()
-
-function lightsToByteString(ledsColorArray) {
-    let bytes = _.flatten(ledsColorArray);
-    return Buffer.from(bytes).toString('base64');
-}
 
 exports.createRemoteControl = function createRemoteControl(lightProgram, deviceMultiplexer) {
   const wss = new WebSocket.Server({ port: 8080 });
 
-  const sound = new EventEmitter();
+  const micConfig = new MicConfig({
+    sendingMicData: true,
+    metric: "Rms"
+  });
 
-  let lastVolumes = [];
-  let lastRawVolumes = [];
-  let lastBands = [];
-
-  let micConfig = {
-      sendingMicData: true,
-      metric: "Rms"
-  }
-
-  let flushVolume = _.throttle(() => {
-      if (micConfig.sendingMicData) {
-          sound.emit('micSample', lastVolumes)
-      }
-      lastVolumes = [];
-  }, 100)
-
-  let avg = 1;
-  let last = new Date();
-
-  soundEmitter.on('processedaudioframe', (frame) => {
-      let timeSinceLastFrame = new Date() - last;
-      if(timeSinceLastFrame > 50) {
-          // console.log(`SOUND DROPPING FRAMES: Last processedaudioframe frame: ${timeSinceLastFrame}ms ago`.red)
-      }
-      last = new Date()
-
-      let {center: {filteredBands, movingStats: {rms: {slow: {normalizedValue}}}}} = frame;
-      // lastRawVolumes.push({... _.mapValues(filteredBands, b => b.movingStats.rms.slow.normalizedValue), all: normalizedValue});
-      // lastRawVolumes.push({... _.mapValues(filteredBands, (b,name) => frame.center.summary[name+'Rms']), all: normalizedValue});
-      lastRawVolumes.push({... _.mapValues(filteredBands, (b,name) =>
-          frame.center.summary[name+micConfig.metric]), all: normalizedValue});
-
-      if(lastRawVolumes.length >= avg) {
-          let avgLastVolumes = {
-              bass: _.sum(_.map(lastRawVolumes, 'bass'))/avg,
-              mid: _.sum(_.map(lastRawVolumes, 'mid'))/avg,
-              high: _.sum(_.map(lastRawVolumes, 'high'))/avg,
-              all: _.sum(_.map(lastRawVolumes, 'all'))/avg
-          }
-
-          lastVolumes.push(avgLastVolumes);
-          flushVolume();
-
-          lastRawVolumes.shift();
-      }
-  })
+  const sound = new SoundSource(soundEmitter, micConfig);
 
   wss.on('connection', function connection(ws) {
 
@@ -70,7 +23,9 @@ exports.createRemoteControl = function createRemoteControl(lightProgram, deviceM
       ws.send(JSON.stringify({ type, payload }))
     }
 
-    sound.on('micSample', (lastVolumes) => {
+    const service = new Service(lightProgram, deviceMultiplexer, micConfig, emit);
+
+    sound.listen((lastVolumes) => {
       emit('micSample', lastVolumes)
     })
 
@@ -79,112 +34,32 @@ exports.createRemoteControl = function createRemoteControl(lightProgram, deviceM
 
       switch (type) {
         case 'setMicDataConfig':
-          onSetMicDataConfig(payload)
+          service.onSetMicDataConfig(payload)
           return
         case 'startSamplingLights':
-          onStartSamplingLights(payload)
+          service.onStartSamplingLights(payload)
           return
         case 'stopSamplingLights':
-          onStopSamplingLights(payload)
+          service.onStopSamplingLights(payload)
           return
         case 'restartProgram':
-          onRestartProgram(payload)
+          service.onRestartProgram(payload)
           return
         case 'updateConfigParam':
-          onUpdateConfigParam(payload)
+          service.onUpdateConfigParam(payload)
           return
         case 'setPreset':
-          onSetPreset(payload)
+          service.onSetPreset(payload)
           return
         case 'setCurrentProgram':
-          onSetCurrentProgram(payload)
+          service.onSetCurrentProgram(payload)
           return
       }
     });
 
-    let simulating = false;
+    service.onConnect()
 
-    function broadcastStateChange() {
-      emit('stateChange', {
-        currentProgramName: lightProgram.currentProgramName,
-        currentConfig: lightProgram.getCurrentConfig(),
-        micConfig
-      })
-    }
-
-    emit('completeState', {
-      programs: lightProgram.getProgramsSchema(),
-      currentProgramName: lightProgram.currentProgramName,
-      currentConfig: lightProgram.getCurrentConfig(),
-      micConfig
-    })
-
-    function onSetMicDataConfig(newMicConfig) {
-      if(newMicConfig.sendingMicData === true) {
-        console.log('[ON] Web client receiving MIC data'.green)
-      } else if(newMicConfig.sendingMicData === false) {
-        console.log('[OFF] Web client stopped receiving MIC data'.gray)
-      }
-
-      micConfig = {... micConfig, ... newMicConfig};
-
-      broadcastStateChange(true);
-    }
-
-    function onStartSamplingLights() {
-      console.log('[ON] Web client sampling lights data'.green)
-      simulating = true
-      emit('layout', lightProgram.layout)
-    }
-
-    function onStopSamplingLights() {
-      console.log('[OFF] Web client stopped sampling lights'.gray)
-      simulating = false;
-    }
-
-    function onRestartProgram() {
-      lightProgram.restart();
-    }
-
-    function lightsCbk(lights) {
-      if (simulating) {
-          let encodedColors = lightsToByteString(lights);
-          emit('lightsSample', encodedColors)
-      }
-    }
-
-    console.log("[ON] Remote control connnected".green)
-    lightProgram.onLights(lightsCbk)
-
-    deviceMultiplexer.onDeviceStatus(devicesStatus => emit('devicesStatus', devicesStatus))
-
-    function onUpdateConfigParam(config) {
-      lightProgram.currentProgram.config = config;
-
-      emit('stateChange', {
-        currentProgramName: lightProgram.currentProgramName,
-        currentConfig: lightProgram.getCurrentConfig(),
-        micConfig
-      })
-    }
-
-    ws.on('disconnect', function () {
-        console.log("[OFF] Remote control DISCONNNECTED".gray)
-        lightProgram.removeOnLights(lightsCbk)
-    });
-
-    function onSetPreset(presetName) {
-      let presets = lightProgram.getCurrentPresets();
-      if (presets[presetName]){
-        lightProgram.currentProgram.config = _.extend(lightProgram.getConfig(), presets[presetName]);
-        broadcastStateChange();
-      }
-    }
-
-    function onSetCurrentProgram(programKey) {
-      lightProgram.setCurrentProgram(programKey)
-      broadcastStateChange();
-    }
+    ws.on('disconnect', () => service.onDisconnect());
 
   });
 }
---
+++ server/src/service.js
@@ -0,0 +1,181 @@
+const { Buffer } = require('buffer');
+const _ = require('lodash');
+
+function lightsToByteString(ledsColorArray) {
+    let bytes = _.flatten(ledsColorArray);
+    return Buffer.from(bytes).toString('base64');
+}
+
+exports.MicConfig = class MicConfig {
+    constructor(config) {
+      this.config = config;
+    }
+
+    update(newConfig) {
+      Object.assign(this.config, newConfig);
+    }
+
+    isSendingMicData() {
+      return this.config.isSendingMicData;
+    }
+
+    getMetric() {
+      return this.config.metric;
+    }
+}
+
+exports.SoundSource = class SoundSource {
+    constructor(soundEmitter, micConfig) {
+        this.soundEmitter = soundEmitter;
+        this.micConfig = micConfig;
+    }
+
+    listen(cb) {
+        let micConfig = this.micConfig;
+        let lastVolumes = [];
+        let lastRawVolumes = [];
+
+        let flushVolume = _.throttle(() => {
+            if (micConfig.isSendingMicData()) {
+                cb(lastVolumes)
+            }
+            lastVolumes = [];
+        }, 100)
+
+        let avg = 1;
+        let last = new Date();
+
+        this.soundEmitter.on('processedaudioframe', (frame) => {
+            let timeSinceLastFrame = new Date() - last;
+            if (timeSinceLastFrame > 50) {
+                // console.log(`SOUND DROPPING FRAMES: Last processedaudioframe frame: ${timeSinceLastFrame}ms ago`.red)
+            }
+            last = new Date()
+
+            let {center: {filteredBands, movingStats: {rms: {slow: {normalizedValue}}}}} = frame;
+            // lastRawVolumes.push({... _.mapValues(filteredBands, b => b.movingStats.rms.slow.normalizedValue), all: normalizedValue});
+            // lastRawVolumes.push({... _.mapValues(filteredBands, (b,name) => frame.center.summary[name+'Rms']), all: normalizedValue});
+            lastRawVolumes.push({... _.mapValues(filteredBands, (b,name) =>
+                frame.center.summary[name+micConfig.getMetric()]), all: normalizedValue});
+
+            if(lastRawVolumes.length >= avg) {
+                let avgLastVolumes = {
+                    bass: _.sum(_.map(lastRawVolumes, 'bass'))/avg,
+                    mid: _.sum(_.map(lastRawVolumes, 'mid'))/avg,
+                    high: _.sum(_.map(lastRawVolumes, 'high'))/avg,
+                    all: _.sum(_.map(lastRawVolumes, 'all'))/avg
+                }
+
+                lastVolumes.push(avgLastVolumes);
+                flushVolume();
+
+                lastRawVolumes.shift();
+            }
+        })
+    }
+}
+
+exports.Service = class Service {
+
+    constructor(lightProgram, deviceMultiplexer, micConfig, emit) {
+        this.lightProgram = lightProgram;
+        this.deviceMultiplexer = deviceMultiplexer;
+        this.micConfig = micConfig;
+        this.emit = emit
+        this.simulating = false;
+    }
+
+    onConnect() {
+        console.log("[ON] Remote control connnected".green)
+
+        const lightProgram = this.lightProgram;
+
+        this.emit('completeState', {
+            programs: lightProgram.getProgramsSchema(),
+            currentProgramName: lightProgram.currentProgramName,
+            currentConfig: lightProgram.getCurrentConfig(),
+            micConfig: this.micConfig.config
+        })
+    
+        lightProgram.onLights(this.lightsCallback)
+    
+        this.deviceMultiplexer.onDeviceStatus(devicesStatus =>
+            this.emit('devicesStatus', devicesStatus))
+    }
+
+    lightsCallback = (lights) => {
+        if (this.simulating) {
+          let encodedColors = lightsToByteString(lights);
+          this.emit('lightsSample', encodedColors)
+        }
+    }
+
+    broadcastStateChange() {
+        const lightProgram = this.lightProgram;
+        this.emit('stateChange', {
+          currentProgramName: lightProgram.currentProgramName,
+          currentConfig: lightProgram.getCurrentConfig(),
+          micConfig: this.micConfig.config
+        })
+      }
+
+    onSetMicDataConfig(newMicConfig) {
+        if (newMicConfig.sendingMicData === true) {
+          console.log('[ON] Web client receiving MIC data'.green)
+        } else if (newMicConfig.sendingMicData === false) {
+          console.log('[OFF] Web client stopped receiving MIC data'.gray)
+        }
+  
+        this.micConfig.update(newMicConfig);
+  
+        this.broadcastStateChange();
+      }
+
+    onSetPreset(presetName) {
+        const lightProgram = this.lightProgram;
+        const presets = lightProgram.getCurrentPresets();
+
+        if (presets[presetName]){
+          lightProgram.currentProgram.config = _.extend(
+              lightProgram.getConfig(), presets[presetName]);
+
+          this.broadcastStateChange();
+        }
+      }
+  
+    onSetCurrentProgram(programKey) {
+      this.lightProgram.setCurrentProgram(programKey)
+      this.broadcastStateChange();
+    }
+
+    onUpdateConfigParam(config) {
+        const lightProgram = this.lightProgram
+        lightProgram.currentProgram.config = config;
+  
+        this.emit('stateChange', {
+          currentProgramName: lightProgram.currentProgramName,
+          currentConfig: lightProgram.getCurrentConfig(),
+          micConfig: this.micConfig.config
+        })
+      }
+
+    onStartSamplingLights() {
+      console.log('[ON] Web client sampling lights data'.green)
+      this.simulating = true;
+      this.emit('layout', this.lightProgram.layout)
+    }
+
+    onStopSamplingLights() {
+      console.log('[OFF] Web client stopped sampling lights'.gray)
+      this.simulating = false;
+    }
+
+    onRestartProgram() {
+      this.lightProgram.restart();
+    }
+
+    onDisconnect() {
+        console.log("[OFF] Remote control DISCONNNECTED".gray)
+        this.lightProgram.removeOnLights(this.lightsCallback)
+    }
+}

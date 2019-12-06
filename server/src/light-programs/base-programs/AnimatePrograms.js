ec9d1d015874234887781859094dfe4660e1d7b4
Federico Bond <federicobond@gmail.com>
2019-12-05 23:47:50 -0300

    Forward audio data in AnimateParam.drawFrame

--- server/src/light-programs/base-programs/AnimatePrograms.js
+++ server/src/light-programs/base-programs/AnimatePrograms.js
@@ -5,12 +5,12 @@ module.exports = function animateParamProgram(
   change
 ) {
   return class AnimateParam extends Program {
-    drawFrame(draw) {
+    drawFrame(draw, audio) {
       this.count = (this.count || 0) + 1;
       if (this.count % frequency == 0) {
         this.config[parameter] = change.apply(this, [this.config[parameter]]);
       }
-      super.drawFrame(draw);
+      super.drawFrame(draw, audio);
     }
   };
 };

2f0153fe2ee47e5df8d21aab8326ceaa47fe0d69
Federico Bond <federicobond@gmail.com>
2019-12-07 13:47:50 -0300

    Implement crossfade in MultiProgram

--- server/src/light-programs/base-programs/MultiPrograms.js
+++ server/src/light-programs/base-programs/MultiPrograms.js
@@ -1,4 +1,9 @@
 const _ = require("lodash");
+const ColorUtils = require("../utils/ColorUtils");
+
+function clamp(v, min, max) {
+  return Math.max(min, Math.min(max, v));
+}
 
 module.exports = function createMultiProgram(
   programSchedule,
@@ -11,10 +16,10 @@ module.exports = function createMultiProgram(
       this.programSchedule = []
         .concat(programSchedule)
         .map(item => _.extend({}, item));
-      this.nextPosition = 0;
+      this.position = 0;
       this.nextStartChange = null;
       this.config = config;
-      this.past = null;
+      this.previous = null;
       this.current = null;
 
       // instantiate each program
@@ -32,25 +37,62 @@ module.exports = function createMultiProgram(
     drawFrame(draw, audio) {
       // init
       if (this.current === null) {
-        let scheduleItem = this.programSchedule[this.nextPosition];
+        let scheduleItem = this.programSchedule[this.position];
         this.current = scheduleItem.programInstance;
         this.nextStartChange = Date.now() + scheduleItem.duration;
       }
 
-      // draw
-      this.current.drawFrame(draw, audio);
+      if (this.previous) {
+        let previousColors, currentColors;
 
-      // TODO: implement crossfade
+        // TODO: remove this forwarding somehow
+        this.previous.timeInMs = this.timeInMs;
+        this.previous.frameNumber = this.frameNumber;
+        this.current.timeInMs = this.timeInMs;
+        this.current.frameNumber = this.frameNumber;
+
+        this.previous.drawFrame((colors) => previousColors = colors, audio);
+        this.current.drawFrame((colors) => currentColors = colors, audio);
+
+        let alpha = clamp(
+          (Date.now() - this.crossFadeStart)
+           / (this.crossFadeFinish - this.crossFadeStart), 0, 1);
+
+        let colors = new Array(currentColors.length);
+        for (let i = 0; i < currentColors.length; i++) {
+          colors[i] = ColorUtils.mix(previousColors[i], currentColors[i], alpha);
+        }
+
+        draw(colors)
+      } else {
+        // TODO: remove this forwarding somehow
+        this.current.timeInMs = this.timeInMs;
+        this.current.frameNumber = this.frameNumber;
+
+        this.current.drawFrame(draw, audio);
+      }
+
+      if (this.crossFadeFinish && Date.now() >= this.crossFadeFinish) {
+        this.crossFadeStart = null;
+        this.crossFadeFinish = null;
+        this.previous = null;
+      }
+
       if (Date.now() >= this.nextStartChange) {
+        // start crossfade
+        this.crossFadeStart = Date.now();
+        this.crossFadeFinish = Date.now() + Math.random() * this.programSchedule[this.position].duration / 5;
+        this.previous = this.current;
+
         if (random) {
-          this.nextPosition = Math.floor(
+          this.position = Math.floor(
             Math.random() * this.programSchedule.length
           ) % this.programSchedule.length;
         } else {
-          this.nextPosition = this.nextPosition + 1 % this.programSchedule.length;
+          this.position = (this.position + 1) % this.programSchedule.length;
         }
 
-        let scheduleItem = this.programSchedule[this.nextPosition]
+        let scheduleItem = this.programSchedule[this.position]
         this.current = scheduleItem.programInstance;
         this.nextStartChange = Date.now() + scheduleItem.duration;
       }

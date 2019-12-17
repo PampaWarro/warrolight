8f414b84ef44a61357c67986b61076f26e8fd096
Federico Bond <federicobond@gmail.com>
2019-12-17 01:04:49 -0300

    Add label with colors when compiling all programs

--- arduino/compile_all.sh
+++ arduino/compile_all.sh
@@ -2,12 +2,25 @@
 # Compile every sketch in the arduino/ directory. Useful for quickly making
 # sure we did not break the build. Soon to be integrated in CI.
 
-set -e
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
 
-die () {  echo "error: $1"; exit 1; }
+die () { >&2 echo "error: $1"; exit 1; }
 
 command -v arduino-cli > /dev/null || die "arduino-cli not installed."
 
+code=0
+
 for sketch in */ ; do
+  echo "${GREEN}Compiling sketch in $sketch${RESET}"
   arduino-cli compile --fqbn arduino:avr:uno "$sketch"
+  if [ $? -ne 0 ]; then
+    code=1
+  fi
 done
+
+if [ $code -ne 0 ]; then
+  echo "${RED}Failed to compile some programs :(${RESET}"
+  exit $code
+fi

30721cc83a9c05859e322dd2c7a6823a0d1469a0
Federico Bond <federicobond@gmail.com>
2019-12-18 01:23:47 -0300

    Remove unnecessary includes from arduino program

--- arduino/strip-alone/lib-warro-light-strip.ino
+++ arduino/strip-alone/lib-warro-light-strip.ino
@@ -1,22 +1,3 @@
-#include <bitswap.h>
-#include <chipsets.h>
-#include <color.h>
-#include <colorpalettes.h>
-#include <colorutils.h>
-#include <controller.h>
-#include <FastLED.h>
-#include <fastled_config.h>
-#include <fastled_delay.h>
-#include <fastled_progmem.h>
-#include <fastpin.h>
-#include <hsv2rgb.h>
-#include <led_sysdefs.h>
-#include <lib8tion.h>
-#include <pixelset.h>
-#include <pixeltypes.h>
-#include <platforms.h>
-#include <power_mgt.h>
-
 #include "FastLED.h"
 
 #define STRIP_NUM_LEDS 150

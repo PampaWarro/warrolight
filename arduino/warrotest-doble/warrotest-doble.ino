86e4040bd93ff80207f4446556b616d8a40d262a
Federico Bond <federicobond@gmail.com>
2019-12-23 00:57:34 -0300

    arduino: use a single CRGB array for both strips in warrotest-doble

--- arduino/warrotest-doble/warrotest-doble.ino
+++ arduino/warrotest-doble/warrotest-doble.ino
@@ -13,87 +13,99 @@
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
-CRGB leds2[NUM_LEDS];
 
-void setup() {
+void setup()
+{
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.addLeds<WS2812B, DATA_PIN2, GRB>(leds2, NUM_LEDS);
+  FastLED.addLeds<WS2812B, DATA_PIN2, GRB>(leds, NUM_LEDS, NUM_LEDS);
 
   Serial.begin(576000);
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
-    leds2[i] = CRGB::Black;
-  }
+  FastLED.showColor(CRGB::Black);
 
-  leds[0] = CRGB::Black;
   leds[1] = CRGB::Red;
   leds[2] = CRGB::Green;
   leds[3] = CRGB::Blue;
 
-  leds2[1] = CRGB::Red;
-  leds2[2] = CRGB::Green;
-  leds2[3] = CRGB::Blue;
+  leds[NUM_LEDS + 1] = CRGB::Red;
+  leds[NUM_LEDS + 2] = CRGB::Green;
+  leds[NUM_LEDS + 3] = CRGB::Blue;
 
   FastLED.show();
 }
 
-void writeLeds(int pos, byte r, byte g, byte b) {
-  if (pos < 150) {
-    leds[pos].setRGB(r, g, b);
-  } else {
-    leds2[pos - 150].setRGB(r, g, b);
-  }
+void writeLeds(int pos, byte r, byte g, byte b)
+{
+  leds[pos].setRGB(r, g, b);
 }
 
-void loop() {
+void loop()
+{
   int c = 0;
   int stripSize = NUM_LEDS;
-  if (Serial.available() >= 2) {
+  if (Serial.available() >= 2)
+  {
     int encoding = Serial.read();
-    if (encoding == ENCODING_POS_RGB) {
+    if (encoding == ENCODING_POS_RGB)
+    {
       int j = Serial.read();
       char data[4 * j];
       int total = Serial.readBytes(data, 4 * j);
-      if (total == 4 * j) {
-        for (int i = 0; i < stripSize; i++) {
+      if (total == 4 * j)
+      {
+        for (int i = 0; i < stripSize; i++)
+        {
           leds[i] = CRGB::Black;
         }
-        for (int i = 0; i < j; i++) {
+        for (int i = 0; i < j; i++)
+        {
           int pos = data[0 + i * 4];
           writeLeds(pos, data[1 + i * 4], data[2 + i * 4], data[3 + i * 4]);
         }
       }
-    } else if (encoding == ENCODING_POS_VGA) {
+    }
+    else if (encoding == ENCODING_POS_VGA)
+    {
       int j = Serial.read();
       char data[2 * j];
       int total = Serial.readBytes(data, 2 * j);
-      if (total == 2 * j) {
-        for (int i = 0; i < stripSize; i++) {
+      if (total == 2 * j)
+      {
+        for (int i = 0; i < stripSize; i++)
+        {
           leds[i] = CRGB::Black;
         }
-        for (int i = 0; i < j; i++) {
+        for (int i = 0; i < j; i++)
+        {
           int pos = data[0 + i * 2];
           byte vga = data[1 + i * 2];
           writeLeds(pos, vgaRed(vga), vgaGreen(vga), vgaBlue(vga));
         }
       }
-    } else if (encoding == ENCODING_VGA) {
+    }
+    else if (encoding == ENCODING_VGA)
+    {
       int j = stripSize;
       char data[j];
       int readTotal = Serial.readBytes(data, j);
-      if (readTotal == j) {
-        for (int i = 0; i < j; i++) {
+      if (readTotal == j)
+      {
+        for (int i = 0; i < j; i++)
+        {
           byte vga = data[i];
           writeLeds(i, vgaRed(vga), vgaGreen(vga), vgaBlue(vga));
         }
       }
-    } else if (encoding == ENCODING_RGB) {
+    }
+    else if (encoding == ENCODING_RGB)
+    {
       int j = stripSize;
       char data[3 * j];
       int total = Serial.readBytes(data, 3 * j);
-      if (total == 3 * j) {
-        for (int i = 0; i < j; i++) {
+      if (total == 3 * j)
+      {
+        for (int i = 0; i < j; i++)
+        {
           writeLeds(i, data[i * 3], data[1 + i * 3], data[2 + i * 3]);
         }
       }

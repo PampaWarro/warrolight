235bc85a4c996e776081d53404b59a39d90b4406
Federico Bond <federicobond@gmail.com>
2019-12-27 01:36:45 -0300

    server: improve noise program

--- server/src/light-programs/programs/noise.js
+++ server/src/light-programs/programs/noise.js
@@ -11,35 +11,41 @@ function rescale(x, min, max) {
 module.exports = class Noise extends LightProgram {
   constructor(config, geometry) {
     super(config, geometry);
-    this.simplex2 = new tumult.Simplex2('warro')
   }
 
   init() {
     this.time = 0;
+    this.noise = new tumult[this.config.noise]()
   }
 
   drawFrame(draw) {
     this.time += this.config.speed;
 
-    const simplex2 = this.simplex2;
+    const noise = this.noise;
     const { x, y } = this.geometry;
     const offset = this.config.offset;
     const t = this.time / 1000;
 
     var colors = new Array(this.numberOfLeds);
     for (let i = 0; i < colors.length; i++) {
-        const r = simplex2.gen(x[i] / 32 + t, y[i] / 32 + t)
-        const g = simplex2.gen(x[i] / 32 + t, y[i] / 32 + t + 100)
-        const b = simplex2.gen(x[i] / 32 + t, y[i] / 32 + t + 200)
-        colors[i] = [r, g, b].map(v => rescale(v, 0, 200));
+        const r = noise.gen(x[i] / 32 + t, y[i] / 32 + t)
+        const g = noise.gen(x[i] / 32 + t, y[i] / 32 + t + 100)
+        const b = noise.gen(x[i] / 32 + t, y[i] / 32 + t + 200)
+        colors[i] = [r, g, b].map(v => rescale(v, 0, 255));
     }
     draw(colors);
+  }
+
+  updateConfig(config) {
+    super.updateConfig(config);
+    this.noise = new tumult[this.config.noise]()
   }
 
   static configSchema() {
     let config = super.configSchema();
     config.speed = { type: Number, min: 1, max: 30, default: 5 };
     config.offset = { type: Number, min: 1, max: 100, default: 13 };
+    config.noise = { type: String, values: ["Simplex2", "Perlin2"], default: "Simplex2" };
     return config;
   }
 };

fefa6a86f41acd9ed632d513e273293f022344b4
Martin Blech <78768+martinblech@users.noreply.github.com>
2019-12-29 13:04:06 -0800

    Python audio stack.
    Haven't integrated it with the lights server yet, it's running in a
    separate playground.

---
+++ audio/input.js
@@ -0,0 +1,74 @@
+const cp = require('child_process');
+const EventEmitter = require('events');
+const path = require('path');
+const msgpack = require('msgpack');
+
+const venvPath = cp.execSync('pipenv --venv', {encoding : 'utf8'}).trim();
+const python = path.join(venvPath, 'bin', 'python')
+const mainScript = 'main.py'
+
+class AudioInput extends EventEmitter {
+  constructor(options) {
+    super();
+    // this._sampleRate = options._sampleRate || 48000;
+    const deviceIndex = options.deviceIndex || null;
+    const profile = options.profile || null;
+    if (!profile) {
+      this._args = [ mainScript ];
+    } else {
+      const profilePath = options.profilePath || 'audioprofile.pstats';
+      this._args = [ '-m', 'cProfile', '-o', profilePath, mainScript ]
+    }
+    if (deviceIndex) {
+      this._args.push('-d', deviceIndex.toString());
+    }
+    this._subprocess = null;
+    this._stopping = false;
+  }
+
+  start() {
+    if (this._subprocess) {
+      throw 'AudioInput already started';
+    }
+    this._stopping = false;
+    const that = this;
+    this._subprocess = cp.spawn(python, this._args, {
+      stdio : [ 'inherit', 'pipe', 'inherit' ],
+    });
+    this._inputStream = new msgpack.Stream(this._subprocess.stdout);
+    this._inputStream.on('msg', (frame) => { that.emit('audioframe', frame); });
+    this.emit('start');
+    this._subprocess.on('exit', (code, signal) => {
+      if (!that._stopping) {
+        let msg;
+        if (code !== null) {
+          msg = `Audio subprocess exited with code ${code}.`;
+        } else {
+          msg = `Audio subprocess terminated with signal ${signal}.`;
+        }
+        that.emit('error', msg);
+      }
+      that._subprocess = null;
+      that.emit('stop');
+    });
+  }
+
+  stop() {
+    this._stopping = true;
+    if (!this._subprocess.kill()) {
+      console.error('Failed to kill audio subprocess.');
+    } else {
+      this._subprocess = null;
+    }
+  }
+}
+
+function listDevices() {
+  const command = [ python, mainScript, '-l' ].join(' ')
+  return cp.execSync(command, {encoding : 'utf8'}).trim();
+}
+
+module.exports = {
+  AudioInput,
+  listDevices,
+}
---
+++ audio/main.py
@@ -0,0 +1,92 @@
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s %(name)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'default',
+        }
+    },
+    'loggers': {},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    }
+})
+
+import argparse
+import sounddevice as sd
+import numpy as np
+import logging
+import sys
+from processor import RawAudioProcessor
+import msgpack
+
+logger = logging.getLogger(__name__)
+
+
+def encode(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    if isinstance(obj, np.float32):
+        return obj.item()
+    return obj
+
+
+def write_output_frame(frame):
+    sys.stdout.buffer.write(
+        msgpack.packb(frame, use_bin_type=True, default=encode))
+    sys.stdout.buffer.flush()
+
+
+def audio_loop(device=None, sample_rate=48000, frame_size=512):
+    processor = RawAudioProcessor(sample_rate)
+    with sd.InputStream(device=device,
+                        samplerate=sample_rate,
+                        blocksize=frame_size,
+                        channels=1,
+                        latency='low') as input_stream:
+        while True:
+            data, overflowed = input_stream.read(frame_size)
+            if overflowed:
+                logger.error('Audio buffer overflowed.')
+            frame = processor.process_raw_audio(data)
+            write_output_frame(frame)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Read and process audio input.')
+    parser.add_argument('-d',
+                        '--device',
+                        type=int,
+                        help='Index of the input device',
+                        default=None)
+    parser.add_argument('-l',
+                        '--list_devices',
+                        action='store_true',
+                        help='Show list of audio devices and exit')
+    args = parser.parse_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        return
+    if args.device is None:
+        device = sd.query_devices(kind='input')
+    else:
+        device = sd.query_devices(args.device)
+    logger.info('Using device "%s".', device['name'])
+    try:
+        audio_loop(args.device)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()

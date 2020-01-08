ba60bf39359cd7c730531e947c79f96d3709ce3d
Martin Blech <78768+martinblech@users.noreply.github.com>
2020-01-07 21:52:39 -0800

    Gradient library, colorize drawable and example program.

--- server/src/light-programs/utils/ColorUtils.js
+++ server/src/light-programs/utils/ColorUtils.js
@@ -129,6 +129,8 @@ function mod(x, m) {
   return ((x % m) + m) % m;
 }
 
+function luminance(r, g, b) { return 0.299 * r + 0.587 * g + 0.114 * b; }
+
 module.exports = {
   mix,
   rgbToHex,
@@ -138,5 +140,6 @@ module.exports = {
   hexToRgb,
   clamp,
   dim,
-  mod
+  mod,
+  luminance,
 }

bccc77e36afd91276e44dabc1cbfec242226d213
= <=>
2020-02-13 23:21:41 -0300

    Switch from css to sass, fix mobile viewport

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "jquery";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "./index.css";
+import "./index.scss";
 import { App } from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));

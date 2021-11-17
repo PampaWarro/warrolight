ad27b0e0e68a8e920837877e4fdc95c47a9750cf
jsilveira <jsilveira@gmail.com>
2021-11-17 01:03:52 -0300

    Make aprox. planar projection of 3D geometry keeping edge lengths

--- server/setups/geometries/conga.js
+++ server/setups/geometries/conga.js
@@ -30,28 +30,76 @@ const HALF_LEDS = LEDS_LONG / 2;
 
 // El orden de los segmentos es clave. Replica cómo vamos a conectar las luces y el orden natural de esos 600 leds
 
+// module.exports = [
+//   // Led 1
+//   Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 4 * PORTION, HEIGHT - 0, LEDS_LONG),
+//   Stripe.old2d(4*PORTION, HEIGHT, PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
+//   Stripe.old2d(PORTION, 2*HEIGHT+PORTION, 0, 4*HEIGHT+PORTION, LEDS_LONG),
+//
+//   // Led 2
+//   Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 2*WIDTH+4*PORTION, HEIGHT - 0, LEDS_LONG - 1),
+//   Stripe.old2d(2*WIDTH+4*PORTION, HEIGHT, 2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
+//   Stripe.old2d(2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, 2*WIDTH+8*PORTION, 4*HEIGHT+PORTION, LEDS_LONG),
+//
+//  // Center
+//   Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
+//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
+//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG),
+//   Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
+//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
+//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, LEDS_LONG),
+//
+// // Man head
+//   //Stripe.old2d(WIDTH+4*PORTION+SM_TRIANGLE_BASE, -TRIANGLE_BASE+SM_TRIANGLE_BASE, WIDTH+4*PORTION-SM_TRIANGLE_BASE, -SM_TRIANGLE_BASE, LEDS_LONG - 1),
+//   //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
+//   //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG - 1),
+// ];
+
+//
+
+const domeStickLeds = 50;
+
+/**
+ * Handmade planar projection based on figma drawing (extracted from gross real 3d shape)
+ * We define half of the dots, and then mirror them on the X axis
+ *
+ *                         f *-------* f2
+ *                            \     /
+ *                             \ d /
+ *                       c *-----*-----* c2
+ *                        /    /  \     \
+ *                       /    /    \     \
+ *                     /    e*------* e2  \
+ *                  b *                    * b2
+ *                   /                      \
+ *                  /                        \
+ *               a *                          * a2
+ *
+ */
+
+let [a,b,c,d,e,f] = ([[0,0], [76,-133],[198,-222], [350,-253], [272,-118], [272,-388]]).map(([x,y]) => [x*scale, y*scale]);
+
+// Mirror versions of all except (d) which is in the middle
+let [a2,b2,c2,e2,f2] = [a,b,c,e,f].map(([x,y]) => [(2*d[0] - x), y]);
+
 module.exports = [
   // Led 1
-  Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 4 * PORTION, HEIGHT - 0, LEDS_LONG),
-  Stripe.old2d(4*PORTION, HEIGHT, PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
-  Stripe.old2d(PORTION, 2*HEIGHT+PORTION, 0, 4*HEIGHT+PORTION, LEDS_LONG),
+  Stripe.old2d(... a, ... b, domeStickLeds),
+  Stripe.old2d(... b, ... c, domeStickLeds),
+  Stripe.old2d(... c, ... d, domeStickLeds),
+  Stripe.old2d(... d, ... f2, domeStickLeds),
+  Stripe.old2d(... f2, ... f, domeStickLeds),
+  Stripe.old2d(... f, ... d, domeStickLeds),
+  Stripe.old2d(... d, ... e2, domeStickLeds),
+  Stripe.old2d(... e2, ... e, domeStickLeds),
+  Stripe.old2d(... e, ... d, domeStickLeds),
+  Stripe.old2d(... d, ... c2, domeStickLeds),
+  Stripe.old2d(... c2, ... b2, domeStickLeds),
+  Stripe.old2d(... b2, ... a2, domeStickLeds),
 
-  // Led 2
-  Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 2*WIDTH+4*PORTION, HEIGHT - 0, LEDS_LONG - 1),
-  Stripe.old2d(2*WIDTH+4*PORTION, HEIGHT, 2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
-  Stripe.old2d(2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, 2*WIDTH+8*PORTION, 4*HEIGHT+PORTION, LEDS_LONG),
 
- // Center
-  Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-  Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-  Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG),
-  Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
-  Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-  Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, LEDS_LONG),
-
-// Man head
-  //Stripe.old2d(WIDTH+4*PORTION+SM_TRIANGLE_BASE, -TRIANGLE_BASE+SM_TRIANGLE_BASE, WIDTH+4*PORTION-SM_TRIANGLE_BASE, -SM_TRIANGLE_BASE, LEDS_LONG - 1),
-  //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
-  //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG - 1),
-
+  // Man head
+    //Stripe.old2d(WIDTH+4*PORTION+SM_TRIANGLE_BASE, -TRIANGLE_BASE+SM_TRIANGLE_BASE, WIDTH+4*PORTION-SM_TRIANGLE_BASE, -SM_TRIANGLE_BASE, LEDS_LONG - 1),
+    //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
+    //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG - 1),
 ];

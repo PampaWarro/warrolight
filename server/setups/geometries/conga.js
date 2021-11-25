63207a1ec4bd2515714bdcf0f58dcb2cda641c2a
jsilveira <jsilveira@gmail.com>
2021-11-24 22:26:59 -0300

    Cleanup geometry file and center sound-waves on man waist

--- server/setups/geometries/conga.js
+++ server/setups/geometries/conga.js
@@ -14,48 +14,6 @@ const { Stripe } = require("../../src/geometry");
  */
 const scale = 0.1;
 
-const PORTION = 200 * scale;
-const WIDTH = 3 * PORTION;
-const TRIANGLE_BASE = 2*PORTION;
-const SM_TRIANGLE_BASE = PORTION/2;
-const HEIGHT = Math.sqrt(4 * PORTION * PORTION - PORTION * PORTION);
-const HALF_HEIGHT = HEIGHT / 2;
-const HALF_WIDTH = WIDTH / 2;
-
-/**
- * Amount of leds on each part
- */
-const LEDS_LONG = 60;
-const HALF_LEDS = LEDS_LONG / 2;
-
-// El orden de los segmentos es clave. Replica cómo vamos a conectar las luces y el orden natural de esos 600 leds
-
-// module.exports = [
-//   // Led 1
-//   Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 4 * PORTION, HEIGHT - 0, LEDS_LONG),
-//   Stripe.old2d(4*PORTION, HEIGHT, PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
-//   Stripe.old2d(PORTION, 2*HEIGHT+PORTION, 0, 4*HEIGHT+PORTION, LEDS_LONG),
-//
-//   // Led 2
-//   Stripe.old2d(WIDTH+4*PORTION, HEIGHT - HEIGHT, 2*WIDTH+4*PORTION, HEIGHT - 0, LEDS_LONG - 1),
-//   Stripe.old2d(2*WIDTH+4*PORTION, HEIGHT, 2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, LEDS_LONG),
-//   Stripe.old2d(2*WIDTH+7*PORTION, 2*HEIGHT+PORTION, 2*WIDTH+8*PORTION, 4*HEIGHT+PORTION, LEDS_LONG),
-//
-//  // Center
-//   Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG),
-//   Stripe.old2d(WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
-//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG),
-//   Stripe.old2d(WIDTH+4*PORTION-TRIANGLE_BASE, TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, TRIANGLE_BASE, LEDS_LONG),
-//
-// // Man head
-//   //Stripe.old2d(WIDTH+4*PORTION+SM_TRIANGLE_BASE, -TRIANGLE_BASE+SM_TRIANGLE_BASE, WIDTH+4*PORTION-SM_TRIANGLE_BASE, -SM_TRIANGLE_BASE, LEDS_LONG - 1),
-//   //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
-//   //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG - 1),
-// ];
-
-//
 const headSideLeds = 20;
 const domeStickLeds = (600 - headSideLeds * 3) / 12;
 
@@ -103,10 +61,4 @@ module.exports = [
   Stripe.old2d(... d, ... c2, domeStickLeds),
   Stripe.old2d(... c2, ... b2, domeStickLeds),
   Stripe.old2d(... b2, ... a2, domeStickLeds),
-
-
-  // Man head
-    //Stripe.old2d(WIDTH+4*PORTION+SM_TRIANGLE_BASE, -TRIANGLE_BASE+SM_TRIANGLE_BASE, WIDTH+4*PORTION-SM_TRIANGLE_BASE, -SM_TRIANGLE_BASE, LEDS_LONG - 1),
-    //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION, 0, LEDS_LONG - 1),
-    //Stripe.old2d(WIDTH+4*PORTION-SM_TRIANGLE_BASE, -TRIANGLE_BASE, WIDTH+4*PORTION+TRIANGLE_BASE, -TRIANGLE_BASE, LEDS_LONG - 1),
 ];

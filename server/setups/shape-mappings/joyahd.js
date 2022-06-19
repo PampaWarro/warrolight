5fef2e8026b2d321d58108c8108fa9f40125e096
Martin Blech <78768+martinblech@users.noreply.github.com>
2022-06-18 19:18:10 -0700

    More joya shape mappings.

--- server/setups/shape-mappings/joyahd.js
+++ server/setups/shape-mappings/joyahd.js
@@ -15,16 +15,21 @@ const ribCount = 6;
 const ribs = [];
 const oddRibs = [];
 const evenRibs = [];
+const mirrorRibs = _.range(ribCount / 2).map(() => []);
 for (let i = 0; i < ribCount; i++) {
   const offset = globalOffset + i * ribSize;
   const rib = _.range(offset, offset + ribSize);
   shapes[`rib${i + 1}`] = rib;
   ribs.push(rib);
-  ((i % 2 === 0)? oddRibs : evenRibs).push(rib);
+  (i % 2 === 0 ? oddRibs : evenRibs).push(rib);
+  mirrorRibs[i % mirrorRibs.length].push(rib);
 }
 shapes.ribs = _.flatten(ribs);
-shapes.oddRibs = _.flatten(oddRibs);
-shapes.evenRibs = _.flatten(evenRibs);
+shapes["odd-ribs"] = _.flatten(oddRibs);
+shapes["even-ribs"] = _.flatten(evenRibs);
+for (let i = 0; i < mirrorRibs.length; i++) {
+  shapes[`mirror-ribs${i + 1}`] = _.flatten(mirrorRibs[i]);
+}
 globalOffset += shapes.ribs.length;
 
 const hSegmentSize = 150;
@@ -32,17 +37,64 @@ const hSegmentCount = 6;
 const horizontal = [];
 const oddHorizontal = [];
 const evenHorizontal = [];
+const mirrorHorizontal = _.range(ribCount / 2).map(() => []);
 for (let i = 0; i < hSegmentCount; i++) {
   const offset = globalOffset + i * hSegmentSize;
   const hSegment = _.range(offset, offset + hSegmentSize);
   shapes[`horizontal${i + 1}`] = hSegment;
   horizontal.push(hSegment);
-  ((i % 2 === 0)? oddHorizontal : evenHorizontal).push(hSegment);
+  (i % 2 === 0 ? oddHorizontal : evenHorizontal).push(hSegment);
+  mirrorHorizontal[i % mirrorHorizontal.length].push(hSegment);
 }
 shapes.horizontal = _.flatten(horizontal);
-shapes.oddHorizontal = _.flatten(oddHorizontal);
-shapes.evenHorizontal = _.flatten(evenHorizontal);
+shapes["odd-horizontal"] = _.flatten(oddHorizontal);
+shapes["even-horizontal"] = _.flatten(evenHorizontal);
+for (let i = 0; i < mirrorHorizontal.length; i++) {
+  shapes[`mirror-horizontal${i + 1}`] = _.flatten(mirrorHorizontal[i]);
+}
 globalOffset += shapes.horizontal.length;
+
+const hShapes = [];
+const oddHShapes = [];
+const evenHShapes = [];
+const mirrorHShapes = _.range(ribCount / 2).map(() => []);
+for (let i = 0; i < ribCount; i++) {
+  const rib0 = ribs[i];
+  const rib1 = ribs[(i + 1) % ribCount];
+  const h = horizontal[i];
+  const hShape = _.flatten([rib0, rib1, h]);
+  shapes[`hshape${i + 1}`] = hShape;
+  (i % 2 === 0 ? oddHShapes : evenHShapes).push(hShape);
+  mirrorHShapes[i % mirrorHShapes.length].push(hShape);
+}
+shapes["odd-hshapes"] = _.flatten(oddHShapes);
+shapes["even-hshapes"] = _.flatten(evenHShapes);
+for (let i = 0; i < mirrorHShapes.length; i++) {
+  shapes[`mirror-hshapes${i + 1}`] = _.flatten(mirrorHShapes[i]);
+}
+
+const ribHIntersection = 80;
+const oddTriangles = [];
+const evenTriangles = [];
+const mirrorTriangles = _.range(ribCount / 2).map(() => []);
+for (let i = 0; i < ribCount; i++) {
+  const rib0 = ribs[i];
+  const rib1 = ribs[(i + 1) % ribCount];
+  const h = horizontal[i];
+  const triangle = _.flatten([
+    rib0.slice(ribHIntersection),
+    rib1.slice(ribHIntersection),
+    h
+  ]);
+  shapes[`triangle${i + 1}`] = triangle;
+  (i % 2 === 0 ? oddTriangles : evenTriangles).push(triangle);
+  mirrorTriangles[i % mirrorTriangles.length].push(triangle);
+}
+shapes["odd-triangles"] = _.flatten(oddTriangles);
+shapes["even-triangles"] = _.flatten(evenTriangles);
+for (let i = 0; i < mirrorTriangles.length; i++) {
+  shapes[`mirror-triangles${i + 1}`] = _.flatten(mirrorTriangles[i]);
+}
 
 module.exports = function getShapes() {
   return shapes;

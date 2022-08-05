58f585a95b438f26b0058ffd42576efa51c07520
Martin Blech <78768+martinblech@users.noreply.github.com>
2022-08-04 23:53:46 -0700

    AnimateProgram: keep track of init time.
    This is useful for programs that repeat, so that the animation function
    can take the time offset from the beginning.

--- server/src/light-programs/base-programs/AnimatePrograms.js
+++ server/src/light-programs/base-programs/AnimatePrograms.js
@@ -5,10 +5,17 @@ module.exports = function animateParamProgram(
   change
 ) {
   return class AnimateParam extends Program {
+    init() {
+      this.startTimeInMs = null;
+      super.init();
+    }
     drawFrame(leds, context) {
+      if (this.startTimeInMs === null) {
+        this.startTimeInMs = context.timeInMs;
+      }
       this.count = (this.count || 0) + 1;
       if (this.count % frequency == 0) {
-        this.config[parameter] = change.apply(this, [this.config[parameter]]);
+        this.config[parameter] = change.call(this, this.config[parameter], context);
       }
       super.drawFrame(leds, context);
     }

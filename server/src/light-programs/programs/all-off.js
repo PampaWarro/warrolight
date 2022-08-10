26cef14e1cce5b8b1e6a7b857d67c1dc27c8076d
Martin Blech <78768+martinblech@users.noreply.github.com>
2022-08-09 23:28:43 -0700

    Bugfix all-white and all-off
    Avoid repeating the same color instance in each element in the leds
    array. It makes other programs that adjust the values apply the same
    transform multiple times on the same object.

--- server/src/light-programs/programs/all-off.js
+++ server/src/light-programs/programs/all-off.js
@@ -3,6 +3,8 @@ const LightProgram = require("./../base-programs/LightProgram");
 module.exports = class AllOff extends LightProgram {
   // Override base class
   drawFrame(leds, context) {
-    leds.fill([0, 0, 0]);
+    for (let i = 0; i < leds.length; i++) {
+      leds[i] = [0, 0, 0];
+    }
   }
 };
--- server/src/light-programs/programs/all-white.js
+++ server/src/light-programs/programs/all-white.js
@@ -6,13 +6,15 @@ module.exports = class AllWhite extends LightProgram {
   drawFrame(leds, context) {
     // En HSV blanco es (0,0,1)
     let tonoDeBlanco = ColorUtils.HSVtoRGB(0, 0, this.config.brillo);
-    leds.fill(tonoDeBlanco);
+    for (let i = 0; i < leds.length; i++) {
+      leds[i] = tonoDeBlanco.slice();
+    }
   }
 
   // Override and extend config Schema
   static configSchema() {
     let res = super.configSchema();
-    res.brillo = { type: Number, min: 0, max: 1, step: 0.01, default: 0.5 };
+    res.brillo = { type: Number, min: 0, max: 1, step: 0.01, default: 1 };
     return res;
   }
 };

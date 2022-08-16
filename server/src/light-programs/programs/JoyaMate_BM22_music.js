c88d0471e8f0bde84700db27249694291a058b6f
Martin Blech <78768+martinblech@users.noreply.github.com>
2022-08-16 16:16:56 -0700

    Minor updates to JoyaMate music program.
    Some defaults for easily switching modes.

--- server/src/light-programs/programs/JoyaMate_BM22_music.js
+++ server/src/light-programs/programs/JoyaMate_BM22_music.js
@@ -1,5 +1,5 @@
 const createMultiProgram = require("../base-programs/MultiPrograms");
-const {randomSchedule} = require("../joyamate-utils/preset");
+const { randomSchedule } = require("../joyamate-utils/preset");
 
 const timeScale = 1; // RESET to 1 before commet.
 const seconds = (1 / timeScale) * 1000;
@@ -10,21 +10,27 @@ const randomDuration = 5 * seconds;
 class MetaSchedule {
   constructor() {
     this.schedules = {
-      any : randomSchedule(null, baseDuration, randomDuration),
-      low : randomSchedule((program, presetName, config) =>
-                               config.tags &&
-                               config.tags.includes('intensity-low'),
-                           baseDuration, randomDuration),
-      mid : randomSchedule((program, presetName, config) =>
-                               config.tags &&
-                               config.tags.includes('intensity-mid'),
-                           baseDuration, randomDuration),
-      high : randomSchedule((program, presetName, config) =>
-                                config.tags &&
-                                config.tags.includes('intensity-high'),
-                            baseDuration, randomDuration),
+      any: randomSchedule(null, baseDuration, randomDuration),
+      low: randomSchedule(
+        (program, presetName, config) =>
+          config.tags && config.tags.includes("intensity-low"),
+        baseDuration,
+        randomDuration
+      ),
+      mid: randomSchedule(
+        (program, presetName, config) =>
+          config.tags && config.tags.includes("intensity-mid"),
+        baseDuration,
+        randomDuration
+      ),
+      high: randomSchedule(
+        (program, presetName, config) =>
+          config.tags && config.tags.includes("intensity-high"),
+        baseDuration,
+        randomDuration
+      ),
     };
-    this.activeSchedule = 'any';
+    this.activeSchedule = "any";
   }
 
   schedule() {
@@ -35,13 +41,16 @@ class MetaSchedule {
 
 const metaSchedule = new MetaSchedule();
 
-module.exports = class JoyaMusic extends createMultiProgram
-(() => metaSchedule.schedule(), false, 5 * seconds) {
+module.exports = class JoyaMusic extends (
+  createMultiProgram(() => metaSchedule.schedule(), false, 5 * seconds)
+) {
   init() {
     super.init();
     this.masks = {};
     for (const [name, shape] of Object.entries(this.shapeMapping())) {
-      const mask = this.masks[name] = new Array(this.numberOfLeds).fill(false);
+      const mask = (this.masks[name] = new Array(this.numberOfLeds).fill(
+        false
+      ));
       for (const i of shape) {
         mask[i] = true;
       }
@@ -56,38 +65,47 @@ module.exports = class JoyaMusic extends createMultiProgram
     }
     for (let i = 0; i < leds.length; i++) {
       if (!mask[i]) {
-        leds[i] = [ 0, 0, 0 ];
+        leds[i] = [0, 0, 0];
       }
     }
   }
 
   updateConfig(newConfig) {
     metaSchedule.activeSchedule = newConfig.intensity;
-    this.current = null;
     this.startNextProgram();
     super.updateConfig(newConfig);
   }
 
   static configSchema() {
     return Object.assign(super.configSchema(), {
-      intensity : {
-        type : String,
-        default : 'any',
-        values : [ 'low', 'mid', 'high', 'any' ],
+      intensity: {
+        type: String,
+        default: "high",
+        values: ["low", "mid", "high", "any"],
       },
-      mask : {
-        type : String,
-        default : 'vertices',
-        values : [
-          'all',
-          'joya',
-          'mate',
-          'vertices',
-          'joya-odd-triangles',
-          'joya-even-triangles',
-          'joya-top-h',
+      mask: {
+        type: String,
+        default: "vertices",
+        values: [
+          "all",
+          "joya",
+          "mate",
+          "vertices",
+          "joya-odd-triangles",
+          "joya-even-triangles",
+          "joya-top-h",
         ],
       },
     });
+  }
+
+  static presets() {
+    return {
+      default: {},
+      joyaParts: { mask: "joya-odd-triangles", intensity: "mid" },
+      joyaMid: { mask: "joya", intensity: "mid" },
+      mateMid: { mask: "mate", intensity: "mid" },
+      todo: { mask: "all", intensity: "high" },
+    };
   }
 };

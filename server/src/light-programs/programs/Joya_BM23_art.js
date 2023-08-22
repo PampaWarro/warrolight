1f0c1824f3e6420c49e80c81540fd1761a031b64
jsilveira <jsilveira@gmail.com>
2023-08-22 01:02:15 -0300

    Joya_BM23: Add programs for "memorias" and "luz" and several presets

--- server/src/light-programs/programs/Joya_BM23_art.js
+++ server/src/light-programs/programs/Joya_BM23_art.js
@@ -101,6 +101,91 @@ const aurora = createNowPlayingPositionProgram([
   },
 ]);
 
+const s = t => {
+  let [,min, sec] = t.match(/(\d+):(\d+)/);
+  return parseInt(min)*60+parseInt(sec);
+}
+
+const memorias = createNowPlayingPositionProgram([
+  {
+    start: s("00:05"), end: s("01:20"), fadeIn: 30, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaTorchesStars'),}, 'JoyaTorchesStars'),
+  },
+  {
+    start: s("00:45"), end: s("02:50"), fadeIn: 10, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'sound-waves', 'JoyaBottomLento'),}, 'JoyaBottomLento'),
+  },
+  {
+    start: s("01:15"), end: s("02:50"), fadeIn: 10, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'sound-waves', 'JoyaTopLento'),}, 'JoyaTopLento'),
+  },
+  {
+    start: s("01:45"), end: s("02:10"), fadeIn: 4, fadeOut: 2,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaPolarColor'),}, 'JoyaPolarColor'),
+  },
+  // {
+  //   start: s("01:50"), end: s("02:50"), fadeIn: 2, fadeOut: 2,
+  //   program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaMidFlash'),}, 'JoyaMidFlash'),
+  // },
+  {
+    start: s("02:15"), end: s("02:50"), fadeIn: 2, fadeOut: 2,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'FireDotsExplosion'),}, 'FireDotsExplosion'),
+  },
+  {
+    start: s("02:45"), end: s("04:00"), fadeIn: 5, fadeOut: 30,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaTriexplosions'),}, 'JoyaTriexplosions'),
+  },
+  {
+    start: s("03:26"), end: s("04:20"), fadeIn: 15, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'sound-waves', 'JoyaDesdePunta'),}, 'JoyaDesdePunta'),
+  },
+  {
+    start: s("04:10"), end: s("04:26"), fadeIn: 12, fadeOut: 4,
+    program: programsByShape({joya: getPreset('joyahdtotems', 'rays', 'fireFastSound'),}, 'fireFastSound'),
+  },
+  {
+    start: s("04:24"), end: s("05:00"), fadeIn: 2, fadeOut: 2,
+    program: programsByShape({all: getPreset('joyahdtotems', 'musicVolumeDotRandom', 'JoyaSensitive'),}, 'JoyaSensitive'),
+  },
+  {
+    start: s("04:55"), end: s("05:25"), fadeIn: 2, fadeOut: 10, // JoyaBassExplosions
+    program: programsByShape({all: getPreset('joyahdtotems', 'musicExplosions', 'RandomColorPunches'),}, 'JoyaBassExplosions'),
+  },
+  {
+    start: s("05:20"), end: s("05:49"), fadeIn: 15,  fadeOut: 15,
+    // program: programsByShape({all: getPreset('joyahdtotems', 'stars', 'JoyaFewStars'),}, 'JoyaFewStars'),
+    program: programsByShape({all: getPreset('joyahdtotems', 'bandParticles', 'sensitive'),}, 'sensitive'),
+  },
+  {
+    start: s("05:20"), fadeIn: 15,
+    // program: programsByShape({all: getPreset('joyahdtotems', 'stars', 'JoyaFewStars'),}, 'JoyaFewStars'),
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaPuntitaAzul'),}, 'sensitive'),
+  },
+]);
+
+const luz = createNowPlayingPositionProgram([
+  { // intro solo mid
+    start: s("00:00"), end: s("01:00"), fadeIn: 10, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaIntroLuz'),}, 'JoyaIntroLuz'),
+  },
+  { // Aparece Beat
+    start: s("00:50"), end: s("01:35"), fadeIn: 10, fadeOut: 1,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaPinkXRay'),}, 'JoyaBottomLento'),
+  },
+  { // Se va el beat
+    start: s("1:35"), end: s("02:23"), fadeIn: 1, fadeOut: 10,
+    program: programsByShape({all: getPreset('joyahdtotems', 'sound-waves', 'JoyaBottomLento'),}, 'JoyaBottomLento'),
+  },
+  { // Aparece Beat
+    start: s("02:20"), end: s("03:25"), fadeIn: 3, fadeOut: 3,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaRelampeja'),}, 'JoyaBottomLento'),
+  },
+  { // Aparece Beat
+    start: s("03:30"), fadeIn: 1, fadeOut: 20,
+    program: programsByShape({all: getPreset('joyahdtotems', 'mix', 'JoyaPinkBlueExplosions'),}, 'JoyaBottomLento'),
+  },
+]);
+
 const hello = placeholder;
 const key = placeholder;
 const geminis = placeholder;
@@ -110,8 +195,6 @@ const carbon = placeholder;
 const bahia = placeholder;
 const antes = placeholder;
 const savane = placeholder;
-const luz = placeholder;
-const memorias = placeholder;
 const arboles = placeholder;
 const lujo = placeholder;
 const nidos = placeholder;
